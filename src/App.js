b4b96f1695cfa0e5c1d3d12ba5ffe898decbd6a3
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-05-20 02:51:59 +0530

    feat : routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,57 @@
+import { Navbar, RequireAuth } from "./components";
+import { Home, Login, Questions, Result, Rules, Signup } from "./pages";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import logo from "./logo.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<ToastContainer
+				position="bottom-center"
+				autoClose={2000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				theme="colored"
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route
+					path="/rules/:category"
+					element={
+						<RequireAuth>
+							<Rules />
+						</RequireAuth>
+					}
+				/>
+				<Route
+					path="/questions/:category"
+					element={
+						<RequireAuth>
+							<Questions />
+						</RequireAuth>
+					}
+				/>
+				<Route
+					path="/result/:category"
+					element={
+						<RequireAuth>
+							<Result />
+						</RequireAuth>
+					}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
