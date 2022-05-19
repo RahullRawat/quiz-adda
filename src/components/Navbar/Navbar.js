92852b4f9315613eb4ac2a96c7af32828831481b
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-05-20 02:57:45 +0530

    feat : navbar and requireAuth components

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import "./Navbar.css";
+
+export const Navbar = () => {
+	const {
+		authState: { token },
+		logout,
+		authDispatch,
+	} = useAuth();
+
+	const navigate = useNavigate();
+
+	const logoutHandler = async () => {
+		await logout();
+		authDispatch({ type: "LOGOUT" });
+		navigate("/");
+	};
+	return (
+		<div className="nav-bar">
+			<div className="nav-brand-title">
+				<Link to="/">QuizAdda</Link>
+			</div>
+			<div className="nav-links">
+				{token ? (
+					<Link to="/" className="btn btn-nav" onClick={logoutHandler}>
+						Logout
+					</Link>
+				) : (
+					<Link to="/login" className="btn btn-nav">
+						Login
+					</Link>
+				)}
+			</div>
+		</div>
+	);
+};
---
+++ src/components/RequireAuth/RequireAuth.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useLocation, Navigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+
+export const RequireAuth = ({ children }) => {
+	const location = useLocation();
+	const {
+		authState: { token },
+	} = useAuth();
+	return token ? (
+		children
+	) : (
+		<Navigate to="/login" state={{ from: location }} replace />
+	);
+};
